6af3759381a3c0f7129029e02b4e7ed0640ee19b
imrangit09 <mrihussain78@gmail.com>
2024-01-13 12:08:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
